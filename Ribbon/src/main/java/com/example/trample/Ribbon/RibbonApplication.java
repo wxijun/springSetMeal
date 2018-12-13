0c1175f3542633b1ebc5b3b4d831c29a9c85e7e2
wxijun <wxj027@163.com>
2018-12-13 18:03:01 +0800

    add Ribbon

---
+++ Ribbon/src/main/java/com/example/trample/Ribbon/RibbonApplication.java
@@ -0,0 +1,14 @@
+package com.example.trample.Ribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RibbonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RibbonApplication.class, args);
+	}
+
+}
+
---
+++ Ribbon/src/main/java/com/example/trample/Ribbon/controller/ConsumerController.java
@@ -0,0 +1,33 @@
+package com.example.trample.Ribbon.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * @ClassName ConsumerController
+ * @Description TODO
+ * @Author wangxijun
+ * @Date 2018/12/13 17:59
+ * @Version 1.0
+ **/
+@RestController
+public class ConsumerController {
+    //注入负载均衡客户端
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @RequestMapping("/consumer")
+    public String helloConsumer() throws ExecutionException, InterruptedException {
+        //这里是根据配置文件的那个providers属性取的
+        ServiceInstance serviceInstance = loadBalancerClient.choose("providers");
+        //负载均衡算法默认是轮询，轮询取得服务
+        URI uri = URI.create(String.format("http://%s:%s", serviceInstance.getHost(), serviceInstance.getPort()));
+        return uri.toString();
+    }
+}
